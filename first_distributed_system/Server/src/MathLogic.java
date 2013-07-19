053903fcf26bf44f4845225a239b918a65c66824
austin wernli <auzy93wow@hotmail.com>
2013-07-18 22:19:58 -0600

    fixed retrieving file usableClasses.txt

--- first_distributed_system/Server/src/MathLogic.java
+++ first_distributed_system/Server/src/MathLogic.java
@@ -19,6 +19,10 @@ public class MathLogic {
         return answer;
     }
 
+    public int add(int a, int b){
+        return a+b;
+    }
+
     public int subtract(Object[] args) {
         Integer answer = 0;
         for (Object number : args) {
