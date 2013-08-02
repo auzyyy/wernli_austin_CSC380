20398e3decedd4bd3f78516ce985c9958b9820be
austin wernli <auzy93wow@hotmail.com>
2013-08-02 02:18:59 -0600

    I have everything working right... But i can't get past this "Premature end of file" error

--- ws_lunch/soap-for-lunch/src/neumont/SoapSocket.java
+++ ws_lunch/soap-for-lunch/src/neumont/SoapSocket.java
@@ -1,16 +1,21 @@
 package neumont;
 
-import generated.GetRestaurants.Envelope;
+import generated.MenuResponse.Body;
+import generated.MenuResponse.GetMenuItems;
+import generated.MenuResponse.MenuItems;
 
 import javax.servlet.annotation.WebServlet;
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import javax.xml.stream.EventFilter;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -25,41 +30,84 @@ public class SoapSocket extends javax.servlet.http.HttpServlet {
 
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         try {
-            InputStream inputStream = request.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            response.setContentType("text/xml");
+            StringReader reader = inputToStringReader(request.getInputStream());
+            XMLInputFactory xmlif = XMLInputFactory.newInstance();
+            XMLEventReader xmler = xmlif.createXMLEventReader(reader);
+            EventFilter filter = new EventFilter() {
+                public boolean accept(XMLEvent event) {
+                    return event.isStartElement();
+                }
+            };
+            XMLEventReader xmlfer = xmlif.createFilteredReader(xmler, filter);
+            XMLEvent xEvent = null;
+            StartElement envelope = (StartElement) xmlfer.nextEvent();
+            StartElement body = (StartElement) xmlfer.nextEvent();
+            StartElement method = (StartElement) xmlfer.nextEvent();
 
-
-            JAXBContext context = JAXBContext.newInstance(generated.ObjectFactory.class);
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-            Object clientRequest = ((JAXBElement)unmarshaller.unmarshal(br)).getValue();
-            String[] splitString = clientRequest.getClass().toString().split("\\.");
-            System.out.println(clientRequest);
-            String className = splitString[1];
-
-            System.out.println(className);
-
-            if (className.equals("GetRestaurants")) {
+            if (method.getName().getLocalPart().equals("getRestaurants")) {
                 sendRestaurants(response);
-            } else if (className.equals("GetRestaurantsMenu")) {
-                sendRestaurantsMenu(response, (generated.GetRestaurants.Envelope) clientRequest);
-            } else if (className.equals("MenuRequest")) {
-                sendRestaurantsMenu(response, (generated.GetRestaurants.Envelope) clientRequest);
-            } else if (className.equals("RestaurantsRequest")) {
-                sendRestaurants(response);
+                System.out.println("restaurants");
+            } else if (method.getName().getLocalPart().equals("getRestaurantMenu")) {
+                sendRestaurantsMenu(response, request);
+                System.out.println("method");
             }
 
+            request.getInputStream().close();
+
+        } catch (XMLStreamException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    private void sendRestaurantsMenu(javax.servlet.http.HttpServletResponse response, javax.servlet.http.HttpServletRequest request) throws IOException {
+        try {
+            StringReader reader = inputToStringReader(request.getInputStream());
+            JAXBContext unContext = JAXBContext.newInstance(generated.MenuRequest.ObjectFactory.class);
+            Unmarshaller unmarshaller = unContext.createUnmarshaller();
+            generated.MenuRequest.Envelope unEnvelope = (generated.MenuRequest.Envelope) unmarshaller.unmarshal(reader);
+
+            String restaurantName = unEnvelope.getBody().getGetRestaurantMenu().getRestaurantName();
+
+            generated.MenuResponse.Envelope envelope = new generated.MenuResponse.Envelope();
+            generated.MenuResponse.Body body = new Body();
+            generated.MenuResponse.GetMenuItems getMenuItems = new GetMenuItems();
+            generated.MenuResponse.MenuItems item1 = new MenuItems();
+            item1.setName("food1");
+            generated.MenuResponse.MenuItems item2 = new MenuItems();
+            item2.setName("food2");
+            getMenuItems.getMenuItems().add(item1);
+            getMenuItems.getMenuItems().add(item2);
+            body.setGetMenuItems(getMenuItems);
+            envelope.setBody(body);
+
+            StringWriter swriter = new StringWriter();
+            JAXBContext marshallerContext = JAXBContext.newInstance(generated.MenuResponse.ObjectFactory.class);
+            Marshaller marshaller = marshallerContext.createMarshaller();
+            marshaller.marshal(envelope, swriter);
+
+            response.getWriter().write(swriter.toString());
+            response.getWriter().flush();
         } catch (JAXBException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
 
-    private void sendRestaurantsMenu(javax.servlet.http.HttpServletResponse response, Envelope envelope) throws IOException {
-        Scanner scan = new Scanner(this.getClass().getClassLoader().getResourceAsStream("/restaurant-restaurants-soap-response.xml"));
-        String message = "";
-        while (scan.hasNextLine()) {
-            message += scan.nextLine();
+    public StringReader inputToStringReader(InputStream inputStream) {
+        StringReader sw = null;
+        try {
+            BufferedReader responseReader = new BufferedReader(new InputStreamReader(inputStream));
+            String response = "";
+            String responseLine = "";
+            while ((responseLine = responseReader.readLine()) != null) {
+                response += responseLine;
+            }
+
+            sw = new StringReader(response);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        response.getWriter().write(message);
+        return sw;
     }
 
     private void sendRestaurants(javax.servlet.http.HttpServletResponse response) throws IOException {
@@ -69,6 +117,7 @@ public class SoapSocket extends javax.servlet.http.HttpServlet {
             message += scan.nextLine();
         }
         response.getWriter().write(message);
+        response.getWriter().flush();
     }
 
 
